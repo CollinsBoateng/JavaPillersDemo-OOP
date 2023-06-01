3027a13547e6743926eced10c18fad20272381d2
Collins K. Boateng <135178353+CollinsBoateng@users.noreply.github.com>
2023-06-01 01:26:50 -0400

    Update ThreadOdd.java

---
+++ ThreadOdd.java
@@ -0,0 +1,36 @@
+package Week3Evaluation;
+
+import java.util.Scanner;
+
+public class ThreadOdd extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println("Please enter an odd integer: ");
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		System.out.println("-------------------------");
+		for (int i = 1; i < 10; i++) {
+
+			if (x % 2 != 0) {
+
+				System.out.println(x);
+				x++;
+				x++;
+			} else {
+				System.out.println("Sorry, your number is even!");
+				break;
+			}
+
+			try {
+				Thread.sleep(1000);
+				System.out.println(Thread.currentThread().getName());
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		}
+
+	}
+}
